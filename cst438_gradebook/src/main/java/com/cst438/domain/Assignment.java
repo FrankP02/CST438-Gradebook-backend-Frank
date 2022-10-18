94c42d49eac0c60ec06d9c251492bb96a99479a2
FrankP02 <frankperez045.a@gmail.com>
2022-10-18 08:25:41 -0700

    updated functions

--- cst438_gradebook/src/main/java/com/cst438/domain/Assignment.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/Assignment.java
@@ -44,6 +44,9 @@ public class Assignment {
 	public Date getDueDate() {
 		return dueDate;
 	}
+	public List<AssignmentGrade> getAssignmentGrades() {
+		return assignmentGrades;
+	}
 	public void setDueDate(Date dueDate) {
 		this.dueDate = dueDate;
 	}
